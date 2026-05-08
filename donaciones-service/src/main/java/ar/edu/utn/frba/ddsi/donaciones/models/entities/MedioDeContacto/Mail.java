ed1ae230b00fdfada44dc7aa57fba97bc3291b70
Oriana <obellorinmata@frba.utn.edu.ar>
2026-05-08 16:26:26 -0300

    Creacion de primeras clases para mensajes y medios de contacto

---
+++ donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/MedioDeContacto/Mail.java
@@ -0,0 +1,5 @@
+package ar.edu.utn.frba.ddsi.donaciones.models.entities.MedioDeContacto;
+
+public class Mail extends MedioDeContacto {
+    private String direccionMail;
+}
---
+++ donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/Mensaje/Mensaje.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.ddsi.donaciones.models.entities.Mensaje;
+
+public class Mensaje {
+    private String asunto;
+    private String cuerpo;
+    private TipoDeMensaje tipoDeMensaje;
+}
