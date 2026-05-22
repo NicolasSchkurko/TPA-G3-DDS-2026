174221193f4df47a4e7cbf1215e1a076b55cd91d
N.Schkurko <schkurkogmail@gmail.com>
2026-05-21 22:26:43 -0300

    fixes de tests

--- donaciones-service/src/test/java/ar/edu/utn/frba/ddsi/donaciones/direccion/DireccionTest.java
+++ donaciones-service/src/test/java/ar/edu/utn/frba/ddsi/donaciones/direccion/DireccionTest.java
@@ -2,20 +2,35 @@ package ar.edu.utn.frba.ddsi.donaciones.direccion;
 
 import ar.edu.utn.frba.ddsi.donaciones.models.entities.direccion.Ciudad;
 import ar.edu.utn.frba.ddsi.donaciones.models.entities.direccion.Direccion;
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.direccion.Provincia;
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.direccion.Pais;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DireccionTest {
-  Ciudad ciudad = new Ciudad("Buenos Aires", "CABA", "Argentina");
-  Direccion direccion = new Direccion("Av. Medrano ", di "Lavalle", 951, 2, "A", ciudad);
+  private final Pais pais = new Pais("Argentina");
+  private final Provincia provincia = new Provincia("CABA", pais);
+  private final Ciudad ciudad = new Ciudad("Buenos Aires", provincia);
 
+  @Test
+  @DisplayName("Test se crea direccion correctamente")
+  void test_direccionCorrecta() {
+    Direccion direccion = new Direccion(
+        "Av. Medrano",    // calleUno
+        "Lavalle",        // calleDos
+        951,              // altura (Integer)
+        2,                // piso
+        "A",              // departamento
+        ciudad            // ciudad
+    );
 
-    @Test
-    @DisplayName("Test se crea direccion correctamente")
-    void test_dirrecionCorrecta (){
-assertEquals()"Av.Medrano", direccion.getCalleUno()
-;
-    asserEquals()}
+    assertEquals("Av. Medrano", direccion.getCalleUno());
+    assertEquals("Lavalle", direccion.getCalleDos());
+    assertEquals(951, direccion.getAltura());
+    assertEquals(2, direccion.getPiso());
+    assertEquals("A", direccion.getDepartamento());
+    assertEquals(ciudad, direccion.getCiudad());
+  }
 }
