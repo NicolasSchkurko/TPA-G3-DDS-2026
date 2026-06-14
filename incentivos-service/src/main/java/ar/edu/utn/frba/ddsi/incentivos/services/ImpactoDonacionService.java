06b2fdc6e302c93a71d01b932d9db7af743d0563
Sofia Muñoz <somunoz@frba.utn.edu.ar>
2026-06-14 09:52:52 -0300

    bleh

--- incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/services/ImpactoDonacionService.java
+++ incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/services/ImpactoDonacionService.java
@@ -2,20 +2,25 @@ package ar.edu.utn.frba.ddsi.incentivos.services;
 
 //intellij q haga los import, estoy con el celu
 
-import ar.edu.utn.frba.ddsi.incentivos.models.repositories.RepositorioDonaciones;
-import ar.edu.utn.frba.ddsi.incentivos.models.repositories.RepositorioPerfiles;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImpactoDonacionService {
    
 //prueba cliente
-    private final RestClient restClient;
+    private final RestClient generalClient;
 
-    // Inyección por constructor
-    public PedidoService(RestClient restClient) {
-        this.restClient = restClient;
+    public UsuarioClientService(RestClient generalClient) {
+        this.generalClient = generalClient;
     }
+
+    public PerfilDonacionDTO obtenerPersonaDeDonacionesPorId(UUID id) {
+        return generalClient.get()
+            .uri("http://localhost:8081/personas/persona/{id}", id) //url cambia para estender a q servicio se conecta
+            .retrieve()
+            .body(PerfilDonacionDTO.class);
+    }
+
 
 //no se si necesitemos algo asi para recibir 1 donacion o varias donaciones
     public List<AlgoDTO> obtenerListaDeAlgoDeDonacionesServicio() {
@@ -25,13 +30,5 @@ public class ImpactoDonacionService {
             .body(new ParameterizedTypeReference<List<AlgoDTO>>() {}); 
             // ParameterizedTypeReference porque el retorno es una lista genérica
     }
-
-
-public UsuarioDTO obtenerPersonaDeDonacionesPorId(UUID id) {
-    return restClient.get()
-        .uri("/personas/persona/{id}", id) // Mapea el ID dinámicamente
-        .retrieve()
-        .body(PerfilDonacionDTO.class); // Retorna un solo objeto directo
-}
 
 }
