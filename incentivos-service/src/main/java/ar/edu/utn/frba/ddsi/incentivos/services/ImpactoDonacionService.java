129481001e375a089c931c09a4b8ad7da0c09ca9
Sofia Muñoz <somunoz@frba.utn.edu.ar>
2026-06-14 09:36:07 -0300

    entendiendo como consumir endpoint del servicio de donaciones, estoy con el celu escribiendo el codigo

--- incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/services/ImpactoDonacionService.java
+++ incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/services/ImpactoDonacionService.java
@@ -1,46 +1,37 @@
 package ar.edu.utn.frba.ddsi.incentivos.services;
 
-import ar.edu.utn.frba.ddsi.incentivos.dto.ImpactoDonacionDTO;
-import ar.edu.utn.frba.ddsi.incentivos.models.entities.Mision.ImpactoDonacion;
+//intellij q haga los import, estoy con el celu
 
-import ar.edu.utn.frba.ddsi.incentivos.models.entities.Perfil.Perfil;
 import ar.edu.utn.frba.ddsi.incentivos.models.repositories.RepositorioDonaciones;
 import ar.edu.utn.frba.ddsi.incentivos.models.repositories.RepositorioPerfiles;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ImpactoDonacionService {
-    //cliente
-    private final RepositorioDonaciones donaciones = RepositorioDonaciones.getInstance();
+   
+//prueba cliente
+    private final RestClient restClient;
 
-    private final RepositorioPerfiles perfiles = RepositorioPerfiles.getInstance();
-
-    private final NotificacionesClient notificaciones;
-
-    public ImpactoDonacionService(NotificacionesClient notificaciones) {
-        this.notificaciones = notificaciones;
+    // Inyección por constructor
+    public PedidoService(RestClient restClient) {
+        this.restClient = restClient;
     }
 
-    public Void procesarImpactoDonacion(ImpactoDonacionDTO dto) {
-        ImpactoDonacion donacion = new ImpactoDonacion(
-                // TODO
-                dto.getCadaAtributo()
-        );
-        donaciones.agregarDonacion(donacion);
-
-        Perfil perfilActualizado = this.impactarDonacion(donacion);
-        //TODO tmb activar carga de metricas y actividadMensual
-
-        notificacionesClient.enviarNotificacion(
-                perfilActualizado.mapToDTO()
-            );
-
-        return null;
+//no se si necesitemos algo asi para recibir 1 donacion o varias donaciones
+    public List<AlgoDTO> obtenerListaDeAlgoDeDonacionesServicio() {
+        return restClient.get()
+            .uri("/algos") // Endpoint de donaciones
+            .retrieve()
+            .body(new ParameterizedTypeReference<List<AlgoDTO>>() {}); 
+            // ParameterizedTypeReference porque el retorno es una lista genérica
     }
 
-    public Perfil impactarDonacion(ImpactoDonacion donacion) {
-        Perfil perfilActualizado = perfiles.actualizarPerfil(donacion);
 
-        return perfilActualizado;
-    }
+public UsuarioDTO obtenerPersonaDeDonacionesPorId(UUID id) {
+    return restClient.get()
+        .uri("/personas/persona/{id}", id) // Mapea el ID dinámicamente
+        .retrieve()
+        .body(PerfilDonacionDTO.class); // Retorna un solo objeto directo
+}
+
 }
