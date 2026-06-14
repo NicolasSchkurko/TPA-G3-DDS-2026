80bc16f80923abe17d50bd216d619fb051b21227
Sofia Muñoz <somunoz@frba.utn.edu.ar>
2026-06-14 00:32:03 -0300

    algo que entendi recien, mñn lo reviso

--- incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/controllers/PerfilController.java
+++ incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/controllers/PerfilController.java
@@ -1,35 +1,24 @@
 package ar.edu.utn.frba.ddsi.incentivos.controllers;
 
 import ar.edu.utn.frba.ddsi.incentivos.services.PerfilService;
+import ar.edu.utn.frba.ddsi.incentivos.dto.MetricasActividadDTO
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/personas")
+@RequestMapping("/perfiles") // Coincide con la ruta del Gateway
 public class PerfilController {
-
+//para mostrarle al cliente persona
     private final PerfilService perfilService;
 
     public PerfilController(PerfilService perfilService) {
         this.perfilService = perfilService;
     }
 
-    //endpoints especificos
+    @GetMapping("/perfil")
+    public ResponseEntity<MetricasActividadDTO> obtenerMetricasPerfil() {
 
-//    POST /personas/{idDonacion}
-//    @PostMapping("/{idDonacion}")
-//    public ResponseEntity<Void>
-//    procesarDonacion(
-//            @PathVariable UUID idDonacion) {
-//
-//        perfilService
-//                .procesarDonacion(
-//                        idDonacion);
-//
-//        return ResponseEntity.ok()
-//                .build();
-//    }
-
-//    GET /personas/{uuid}/metricas
-//    GET /personas/{uuid}/misiones
-//    GET /personas/{uuid}/insignias
+        MetricasActividadDTO metricas = new MetricasActividadDTO();
+//logica
+        return ResponseEntity.ok(metricas);
+    }
 }
