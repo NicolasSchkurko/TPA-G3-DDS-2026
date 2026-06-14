e5c137bd082c238fa44576e85aad6069dcd0d2c4
valentin-fondovila <vfondovila@frba.utn.edu.ar>
2026-06-14 13:27:10 -0300

    cambiamos como se reciben y envian notificaciones

--- incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/services/ImpactoDonacionService.java
+++ incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/services/ImpactoDonacionService.java
@@ -2,11 +2,7 @@ package ar.edu.utn.frba.ddsi.incentivos.services;
 
 //intellij q haga los import, estoy con el celu
 
-import ar.edu.utn.frba.ddsi.incentivos.dto.PerfilDonanteDTO;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
-
-import java.util.UUID;
 
 @Service
 public class ImpactoDonacionService {
@@ -14,25 +10,25 @@ public class ImpactoDonacionService {
 //prueba cliente
     private final RestClient generalClient;
 
-    public ImpactoDonacionService(RestClient generalClient) {
+    public UsuarioClientService(RestClient generalClient) {
         this.generalClient = generalClient;
     }
 
-    public PerfilDonanteDTO obtenerPersonaDeDonacionesPorId(UUID id) {
+    public PerfilDonacionDTO obtenerPersonaDeDonacionesPorId(UUID id) {
         return generalClient.get()
             .uri("http://localhost:8081/personas/persona/{id}", id) //url cambia para estender a q servicio se conecta
             .retrieve()
-            .body(PerfilDonanteDTO.class);
+            .body(PerfilDonacionDTO.class);
     }
 
 
 //no se si necesitemos algo asi para recibir 1 donacion o varias donaciones
-//    public List<AlgoDTO> obtenerListaDeAlgoDeDonacionesServicio() {
-//        return restClient.get()
-//            .uri("/algos") // Endpoint de donaciones
-//            .retrieve()
-//            .body(new ParameterizedTypeReference<List<AlgoDTO>>() {});
-//            // ParameterizedTypeReference porque el retorno es una lista genérica
-//    }
+    public List<AlgoDTO> obtenerListaDeAlgoDeDonacionesServicio() {
+        return restClient.get()
+            .uri("/algos") // Endpoint de donaciones
+            .retrieve()
+            .body(new ParameterizedTypeReference<List<AlgoDTO>>() {}); 
+            // ParameterizedTypeReference porque el retorno es una lista genérica
+    }
 
 }
--- incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/services/NotificacionService.java
+++ incentivos-service/src/main/java/ar/edu/utn/frba/ddsi/incentivos/services/NotificacionService.java
@@ -3,8 +3,10 @@ package ar.edu.utn.frba.ddsi.incentivos.services;
 //q los import los haga intellij, estoy en el celu
 
 import ar.edu.utn.frba.ddsi.incentivos.dto.PerfilNotificacionDTO;
+import ar.edu.utn.frba.ddsi.incentivos.models.entities.Perfil.Perfil;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
@@ -16,10 +18,12 @@ public class NotificacionService {
         this.generalClient = generalClient;
     }
 
-    public PerfilNotificacionDTO obtenerContactoDeNotificacionesPorId(UUID id) {
-        return generalClient.post()
-            .uri("http://localhost:8082/notificaciones/{id}", id)
-            .retrieve()
-            .body(PerfilNotificacionDTO.class);
+    public void enviarNotificacion(Perfil perfil) {
+        //TODO crear notificacion a partir de perfil
+        //TODO dto a enviar
+        generalClient.post()
+                .uri("http://localhost:8082/notificaciones/dtoAEnviar", dtoAEnviar)
+                .retrieve()
+                .body(void.class);
     }
 }
